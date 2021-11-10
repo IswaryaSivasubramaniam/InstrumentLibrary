67aa13c85e8ff5d59b156715f1b17c6e77645bbd
Iswarya <24439@student.dorset-college.ie>
2021-11-10 16:40:36 +0000

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using InstrumentLibrary.Models;
+using System;
+
+namespace InstrumentLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Guitar myGuitar=new Guitar("Gibson");
+            Mandolin myMandolin = new Mandolin("Kentucky KM-140");
+            Ukele myUkele = new Ukele("Ortega");
+
+            myUkele.serialNumber = "abc123";
+
+            myMandolin.Play();
+            myGuitar.Play();
+            myUkele.Play();
+        }
+    }
+}
